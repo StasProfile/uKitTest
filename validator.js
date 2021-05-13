b4dd463981cbdf9781383ef9f0eb4f66ba883109
StasProfile <sbabkin@sfedu.ru>
2021-05-13 13:37:46 +0300

    fix

--- validator.js
+++ validator.js
@@ -1,9 +1,32 @@
 const inputArea = document.getElementById('input-area');
 const ceiling = document.getElementById('ceiling');
 
-function validateInputArea(ceiling) {
-    let number = Number(inputArea.value);
-
+function validateInputArea(ceiling){
+    const number = Number(inputArea.value);
+    if (number > ceiling) {
+        inputArea.value = ceiling;
+    }
+    // if (inputArea.value[inputArea.length - 1] !== '.') {
+    //     var i = 0;
+    //     str = inputArea.value;
+    //     console.log(isNaN(str));
+    //     while (isNaN(str)) {
+    //         if (isNaN(str[i] && str[i] !== '.')) {
+    //             str = str.slice(0, str.indexOf(str[i]))
+    //             .concat(str.slice(str.indexOf(str[i]) + 1, str.length));
+    //         }
+    //         i += 1;
+    //     }
+    //     inputArea.value = str;
+    // }
+    
+    if (inputArea.value.indexOf('.') !== inputArea.value.lastIndexOf('.')) {
+        const str = inputArea.value
+        const indexOf = str.lastIndexOf('.')
+        inputArea.value = str.slice(0, indexOf)
+        .concat(str.slice(indexOf + 1, str.length));  
+    }
+    
     if (Number.isNaN(number)) {
         str = inputArea.value;
         for (const c of str){
@@ -14,43 +37,34 @@ function validateInputArea(ceiling) {
             if (Number.isNaN(digit)) {
                 inputArea.value = str.slice(0, str.indexOf(c))
                 .concat(str.slice(str.indexOf(c) + 1, str.length));
+                console.log(inputArea.value)
             }
         }
-        
-    }
-
-    if (inputArea.value.startsWith('00')) {
-        const i = 1;
-        while (inputArea.value[i] === '0') {
-            inputArea.value = inputArea.value.substr(1, inputArea.value.length);
-            if (inputArea.value === '0') {
-                break;
-            }
+        if (isNaN(inputArea.value)) {
+            inputArea.value = '';
         }
-        if (inputArea.value[0] === '0' && inputArea.value[1] !== '.') {
-            inputArea.value = inputArea.value.substr(1, inputArea.value.length);
-        }
-    }
-
-    if (Math.round(number * 100) !== number * 100) {
-        console.log(inputArea.value.slice(0, inputArea.value.indexOf('.') + 3));
-        inputArea.value = inputArea.value.slice(0, inputArea.value.indexOf('.') + 3);
-    }
-
-    number = Number(inputArea.value);
-    if (number > ceiling) {
-        inputArea.value = ceiling
-    }
-
-    if (inputArea.value.indexOf('.') !== (inputArea.value.length - 2) && inputArea.value.endsWith('0')) {
-        inputArea.value = number;
     }
 
     if ((inputArea.value.indexOf('0') === 0 && inputArea.value.indexOf('.') !== 1) && inputArea.value.indexOf('.') !== -1){
         inputArea.value = inputArea.value.slice(1, inputArea.length)
     }
-} 
+    
+    while (inputArea.value.startsWith('00')) {
+            inputArea.value = inputArea.value.slice(1, inputArea.length)
+    }
+
+    if (inputArea.value[0] == '.') {
+        inputArea.value = '';
+    }
+
+    if (Math.round(number * 100) !== number * 100) {
+        inputArea.value = inputArea.value.slice(0, inputArea.value.indexOf('.') + 3);
+    }
+}
 
 inputArea.addEventListener('input', () => {
-    validateInputArea(ceiling.value || 100);
+    if (ceiling.value === ''){
+        ceiling.value = 100;
+    }
+    validateInputArea(ceiling.value);
 })
